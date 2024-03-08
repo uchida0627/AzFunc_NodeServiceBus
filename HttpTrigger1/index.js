1bba512c570c2e0324bd7b7ec3488e1d8f95f5e8
uchida0627 <v-tuchida@microsoft.com>
2024-03-08 17:46:43 +0900

    my first commit

--- HttpTrigger1/index.js
+++ HttpTrigger1/index.js
@@ -1,12 +1,35 @@
-module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+const { ServiceBusClient } = require("@azure/service-bus");
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully.CI/CDは成功しています"
-        : "This HTTP triggered function executed successfully. CI/CDは成功しています"
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
+module.exports = async function (context, req) {
+  context.log("JavaScript HTTP trigger function processed a request.");
+
+  // ServiceBusClientのインスタンスを作成します。以下のエンドポイントは存在しないものです。
+  const client1 = new ServiceBusClient(
+    "Endpoint=sb://uchi-winnode-bus.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=FrvfZ0CBFZoVsXW6ueKF2xC3N1pGLXl/N+ASbNyBLfU="
+  );
+  // const client2 = new ServiceBusClient(
+  //   "Endpoint=sb://uchi-winnode-bus2.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=vh/UsAYKwNyp7XHhRRDxj/eNYaYSFHJiS+ASbPeU1rI="
+  // );
+
+  // ServiceBusSenderのインスタンスを作成します。
+  const sender1 = client1.createSender("myinputqueue");
+  // const sender2 = client2.createSender("myinputqueue2");
+
+  // メッセージを作成します。
+  for (var i = 0; i < 8000; i++) {
+    const message = {
+      body: "Hello, World! from Http Trigger1" + i + "個目のメッセージです。",
     };
-}
+
+    // 2つのServiceBusに同時にメッセージを送信します。
+    await Promise.all([
+      sender1.sendMessages(message),
+      // sender2.sendMessages(message),
+    ]);
+  }
+
+  context.res = {
+    // status: 200, /* Defaults to 200 */
+    body: "Message sent to both ServiceBus instances",
+  };
+};
---
+++ README.md
@@ -0,0 +1,1 @@
+I verified that it is possible to monitor two service buses with a single trigger.
